308bdb611fcfb17821e5c241948138c801bf841f
Amit <theamit.969@gmail.com>
2025-08-31 04:47:36 +0530

    updated

--- car.js
+++ car.js
@@ -1,8 +1,35 @@
 class Car {
   constructor(x, y, width, height) {
     this.x = x;
-    this.x = x;
+    this.y = y;
     this.width = width;
     this.height = height;
+
+    this.controls = new Controls();
+  }
+
+  update() {
+    if (this.controls.forward) {
+      this.y -= 2;
+    }
+    if (this.controls.reverse) {
+      this.y += 2;
+    }
+    if (this.controls.left) {
+      this.x -= 2;
+    }
+    if (this.controls.right) {
+      this.x += 2;
+    }
+  }
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.rect(
+      this.x - this.width / 2,
+      this.y - this.height / 2,
+      this.width,
+      this.height,
+    );
+    ctx.fill();
   }
 }

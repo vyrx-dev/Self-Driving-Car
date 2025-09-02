3978c95af2c1986790cefe307c2274e07ed4d3a7
Amit <theamit.969@gmail.com>
2025-09-03 04:53:26 +0530

    added angle for more realistic look

--- car.js
+++ car.js
@@ -9,6 +9,7 @@ class Car {
     this.acceleration = 0.2;
     this.maxSpeed = 3;
     this.friction = 0.05;
+    this.angle = 0;
 
     this.controls = new Controls();
   }
@@ -42,16 +43,23 @@ class Car {
     if (this.controls.right) {
       this.x += 2;
     }
-    this.y -= this.speed;
+    if (this.controls.left) {
+      this.angle += 0.03;
+    }
+    if (this.controls.right) {
+      this.angle -= 0.03;
+    }
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
   }
   draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+
     ctx.beginPath();
-    ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height,
-    );
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
+    ctx.restore();
   }
 }

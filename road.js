d023b20bcafc2bbdf50a0227409a9b4774902437
Amit <theamit.969@gmail.com>
2025-09-16 01:06:19 +0530

    car lane

--- road.js
+++ road.js
@@ -1,16 +1,20 @@
 class Road {
-  constructor(x, width, laneCount = 3) {
+  constructor(x, width, laneCount = 4) {
     this.x = x;
     this.width = width;
     this.laneCount = laneCount;
     this.left = x - width / 2;
-    this.right = x + width / 2;
+    this.right = x + width / 2
 
     const infinity = 1000000;
     this.top = -infinity;
     this.bottom = infinity;
   }
 
+  getLaneCenter(laneIndex) {
+    const laneWidth = this.width / this.laneCount
+    return this.left + laneWidth / 2 + laneIndex * laneWidth
+  }
   draw(ctx) {
     ctx.lineWidth = 5;
     ctx.strokeStyle = "cyan";
@@ -20,13 +24,17 @@ class Road {
         this.right,
         i / this.laneCount
       );
+
+      if (i > 0 && i < this.laneCount) {
+        ctx.setLineDash([20, 20])
+      } else {
+        ctx.setLineDash([])
+      }
+
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
   }
-}
-function lerp(A, B, t) {
-  return A + (B - A) * t;
 }

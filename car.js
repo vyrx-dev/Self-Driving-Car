b3740bbf83321f2784a69f1f3f8297610671d0a9
Amit <theamit.969@gmail.com>
2026-01-08 06:48:27 +0530

    Implement AI neural network control for self-driving car
    - Add neural network integration with sensor data processing
    - Implement AI control logic for movement decisions
    - Update car to use AI control type instead of manual keys
    - Improve code formatting and structure

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car {
-  constructor(x, y, width, height,controlType, maxSpeed=3) {
+  constructor(x, y, width, height, controlType, maxSpeed = 3) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -11,29 +11,41 @@ class Car {
     this.friction = 0.05;
     this.angle = 0;
     this.damaged = false;
+    this.useBrain = controlType == "AI";
 
     this.img = new Image();
     this.img.src = "images/car.png";
 
-    if(controlType!="DUMMY"){
-        this.sensor = new Sensor(this);
+    if (controlType != "DUMMY") {
+      this.sensor = new Sensor(this);
+      this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
     }
     this.controls = new Controls(controlType);
   }
 
-  update(roadBorders,traffic) {
-    if(!this.damaged){
-
-    this.#move();
-    this.polygon = this.#createPolygon();
-    this.damaged = this.#assessDamage(roadBorders,traffic);
+  update(roadBorders, traffic) {
+    if (!this.damaged) {
+      this.#move();
+      this.polygon = this.#createPolygon();
+      this.damaged = this.#assessDamage(roadBorders, traffic);
     }
-    if(this.sensor){
-        this.sensor.update(roadBorders,traffic);
+    if (this.sensor) {
+      this.sensor.update(roadBorders, traffic);
+      const offsets = this.sensor.readings.map((s) =>
+        s == null ? 0 : 1 - s.offset,
+      );
+      const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+      if (this.useBrain) {
+        this.controls.forward = outputs[0];
+        this.controls.left = outputs[1];
+        this.controls.right = outputs[2];
+        this.controls.reverse = outputs[3];
+      }
     }
   }
 
-  #assessDamage(roadBorders,traffic) {
+  #assessDamage(roadBorders, traffic) {
     for (let i = 0; i < roadBorders.length; i++) {
       if (polysIntersect(this.polygon, roadBorders[i])) {
         return true;
@@ -117,7 +129,7 @@ class Car {
     this.y -= Math.cos(this.angle) * this.speed;
   }
 
-  draw(ctx,color) {
+  draw(ctx, color) {
     if (this.damaged) {
       ctx.fillStyle = "blue";
     } else {
@@ -137,8 +149,8 @@ class Car {
     //   this.width,
     //   this.height,
     // );
-    if(this.sensor){
-    this.sensor.draw(ctx);
+    if (this.sensor) {
+      this.sensor.draw(ctx);
     }
   }
 }
